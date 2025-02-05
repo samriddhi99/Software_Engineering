eed9759116821ab74bfce070fbe67a4af3c0ad06
Samriddhi Singh <samriddhisingh6079@gmail.com>
2025-02-05 08:58:14 +0530

    Add

---
+++ prac.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << "The value of n is " << n << "!" << endl;
+
+    //cout << "This is another sentence";
+
+    return 0; 
+}
